27f24a9c1374508c66dbb3f15d870a5fda34c472
brujua <brunocrisafulli@yahoo.com.ar>
2019-02-25 06:30:26 -0300

    Refactor use Texture for Sprite

--- core/src/com/brujua/defenders/sprites/RandomPassingShipAnimation.java
+++ core/src/com/brujua/defenders/sprites/RandomPassingShipAnimation.java
@@ -3,50 +3,52 @@ package com.brujua.defenders.sprites;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 
 public class RandomPassingShipAnimation {
 
-    private Texture spaceShip;
+    private static final float SPACESHIP_VEL = 200; //kilometers (world unit)
+    private static final double TIME_WITHOUT_SHIP = 0.5; //seconds
+
+    private Sprite spaceShip;
     private Vector2 shipPos;
     private Vector2 shipVel;
-    private static final float SPACESHIP_VEL = 200;
-    private static final double TIME_WITHOUT_SHIP = 0.5;
-    private double timeOutOfView = 0.0;
-    private boolean shipOutOfScreen = false;
-    private float screenWidth;
-    private float screenHeight;
+    private double timeOutOfWorld = 0.0;
+    private boolean shipOutOfWorld = false;
+    private float worldWidth;
+    private float worldHeight;
 
     public RandomPassingShipAnimation(float worldWidth, float worldHeight){
-        this.screenWidth = worldWidth;
-        this.screenHeight = worldHeight;
-        spaceShip = new Texture(Gdx.files.internal("spaceShip.png"));
-        shipPos = new Vector2(0, (float) (screenHeight*1.5/3));
+        this.worldWidth = worldWidth;
+        this.worldHeight = worldHeight;
+        spaceShip = new Sprite(new Texture(Gdx.files.internal("spaceShip.png")));
+        shipPos = new Vector2(0, (float) (worldHeight*1.5/3));
         shipVel = new Vector2((float) (SPACESHIP_VEL*0.75),0);
 
     }
 
     public void update(float delta){
-        if(!shipOutOfScreen){
+        if(!shipOutOfWorld){
             shipPos.add(shipVel.x * delta,shipVel.y*delta);
-            if(shipPos.x > screenWidth || shipPos.x <0 || shipPos.y <0 || shipPos.y > screenHeight ){
-                shipOutOfScreen = true;
-                timeOutOfView = 0;
+            if(shipPos.x > worldWidth || shipPos.x <0 || shipPos.y <0 || shipPos.y > worldHeight ){
+                shipOutOfWorld = true;
+                timeOutOfWorld = 0;
             }
         }
         else {
-            timeOutOfView +=delta;
-            if(timeOutOfView > TIME_WITHOUT_SHIP){
-                shipOutOfScreen = false;
+            timeOutOfWorld +=delta;
+            if(timeOutOfWorld > TIME_WITHOUT_SHIP){
+                shipOutOfWorld = false;
                 setRandomVelAndPosition();
             }
         }
     }
 
     private void setRandomVelAndPosition() {
-        shipPos.x = Math.random()>0.5 ? 0 : screenWidth;
-        shipPos.y = shipPos.y = (float) (100 + Math.random()*400);
+        shipPos.x = Math.random()>0.5 ? 0 : worldWidth;
+        shipPos.y = (float) (100 + Math.random()*400);
         double xVel = Math.random() * SPACESHIP_VEL;
         if(shipPos.x == 0){
             shipVel.x = (float) xVel;
@@ -60,12 +62,13 @@ public class RandomPassingShipAnimation {
     public void render(Batch batch){
         if(shipPos.x >0 && shipPos.y >=0) {
             batch.begin();
-            batch.draw(spaceShip, shipPos.x, shipPos.y);
+            spaceShip.setPosition(shipPos.x,shipPos.y);
+            spaceShip.draw(batch);
             batch.end();
         }
     }
 
     public void dispose() {
-        spaceShip.dispose();
+        spaceShip.getTexture().dispose();
     }
 }
